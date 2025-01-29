8709c303d50a33a0f3c712dc3c369f0d4ab3cd00
Norwood Shane Banzuela <167977392+shanebanz@users.noreply.github.com>
2025-01-29 13:57:07 +0800

    Additional Changes

--- TechnicalActivity1/T1-3a.py
+++ TechnicalActivity1/T1-3a.py
@@ -1,28 +1,43 @@
+print("A). \n")
+
+for i in range(1,6):
+    
+    for j in range(1,6-i):
+        print(" ", end="")
+        
+    for x in range(1, i+1):
+        print(x, end="")
+    
+    print()
+
+print("\nB). ")
+
+#change from for loop to while loop
+i = 0
+while i < 7:
+    j = 0
+    while j <= i:
+        if i == 1:
+            break
+        elif i == 5:
+            break
+        else:
+            print(end="")
+        print(i + 1, end=" ")
+        j += 1
+    print()
+    i += 1
 
 # for i in range(0,5):
 #     for j in range(i+1):
 #         print(j+1, end=" ")
 #     print() 
-    
-    
-# Basic Number Loop
+# basic number loop
 # 1
 # 12
 # 123
 # 1234
 # 12345
-
-
-# for i in range(1,6):
-    
-#     for j in range(1,6-i):
-#         print(" ", end="")
-        
-#     for x in range(1, i+1):
-#         print(x, end="")
-#         # print((i+1-x), end="")
-    
-#     print()
 
 #a).
 #     1
@@ -31,24 +46,4 @@
 #  1234
 # 12345
 
-
-"""
-1
-333
-55555
-666666
-7777777
-this should be the output
-"""
-
-
-for i in range(0,7):
-    for j in range(i+1):
-        if i == 1:
-            break
-        elif i == 5:
-            break
-        else:
-            print(end="")
-        print(i+1, end=" ")
-    print() 
+# print((i+1-x), end="") (I used this for experimental purposes)
