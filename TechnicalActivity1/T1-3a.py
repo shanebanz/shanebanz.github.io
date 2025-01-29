1634d349f56bd940633e3ab6865a9098bf9b0f16
Norwood Shane Banzuela <167977392+shanebanz@users.noreply.github.com>
2025-01-29 13:04:24 +0800

    Technical First Commit

---
+++ TechnicalActivity1/T1-3a.py
@@ -0,0 +1,25 @@
+"""
+for i in range(0,5):
+         for j in range(i+1):
+         print(j+1, end=" ")
+     print()
+"""
+
+""""
+for i in range(1,6):
+    
+    for j in range(1,6-i):
+        print(" ", end="")
+        
+        
+    for x in range(1, i+1):
+        print((i+1-x), end="")
+    
+    print()
+"""
+    
+#     1
+#    21
+#   321
+#  4321
+# 54321
